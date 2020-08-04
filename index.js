1c93b6bd53c131db33e759ada06fc08d61086229
taylorkraft <taylorkraft16@gmail.com>
2020-08-03 18:01:36 -0600

    fetch users function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Success!")
+  fetchUsers()
 })
+
+const BASE_URL = "http://127.0.0.1:3000"
+
+function fetchUsers(){
+  fetch(`${BASE_URL}/users`)
+  .then(res => console.log(res.json()))
+  .then()
+}
